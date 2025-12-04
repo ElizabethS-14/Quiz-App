cb8771ee6d83d80ef290a6d9d6c83b1a6972b494
Elizabeth Sterling <sterling.elizabeth08@gmail.com>
2025-12-04 02:05:33 -0500

    added the string xml file for french so that if the user pics the french button, most things are translated to french

--- app/src/main/java/edu/bpi/moviesbyemojis/MainActivity.java
+++ app/src/main/java/edu/bpi/moviesbyemojis/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.Locale;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
     Button Hard;
 
+    Button englishButton;
+
+    Button frenchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         Easy = (Button) findViewById(R.id.Easy);
         Medium = (Button) findViewById(R.id.Medium);
         Hard = (Button) findViewById(R.id.Hard);
+        englishButton = (Button) findViewById(R.id.englishButton);
+        frenchButton = (Button) findViewById(R.id.frenchButton);
 
         Easy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +70,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        englishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("en");
+            }
+        });
 
+        frenchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeLanguage("fr");
+            }
+        });
+
+
+    }
+
+    private void changeLanguage(String languageCode){
+        //Create a method called changeLanguage that receives a language code
+        Locale locale = new Locale(languageCode);
+        //use this translator as the default one
+        Locale.setDefault(locale);
+
+        //new setting object
+        Configuration config = new Configuration();
+        //use this language translator
+        config.setLocale(locale);
+
+        //get all app texts and update them with the new language setting
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+
+        //saves info when app closes
+        SharedPreferences prefs = getSharedPreferences("Settings", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        //writes down language choice
+        editor.putString("Language", languageCode);
+        //saves what was written
+        editor.apply();
+
+        // Restart the screen
+        recreate();
     }
 }
